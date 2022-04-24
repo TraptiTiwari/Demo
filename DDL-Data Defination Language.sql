20d09e5c8c5ac1559bd11b8aaabd32fa981b9915
TraptiTiwari <104299389+TraptiTiwari@users.noreply.github.com>
2022-04-24 15:00:35 +0530

    Add files via upload

---
+++ DDL-Data Defination Language.sql
@@ -0,0 +1,39 @@
+create database student ;
+
+drop database student;
+
+create table student.records
+(
+rollno varchar(12),
+student_name varchar(30),
+DOJ date,
+DOB date,
+Address varchar(100),
+Mobile bigint,
+Gender varchar(7)
+);
+
+drop table student.records;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
